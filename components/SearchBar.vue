fd6ffe46ff44ca8b8fee1ca1d2dcf808a71e7896
maiwenn <maiwenn@soixantecircuits.fr>
2022-07-15 10:04:16 +0200

    :gem: capture keypress action for search

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,17 +1,34 @@
 <script setup>
 import { useStore } from '../store/index.js'
 
+const ENTER_KEY = 13
 const store = useStore()
 const input = ref('')
 
 const search = () => {
   store.searchWord = input.value
 }
+
+ onMounted(() => {
+  window.addEventListener('keypress', e => {
+    if (e.keyCode === ENTER_KEY) {
+      search()
+    }
+  })
+})
+
+onUnmounted(() => {
+  window.removeEventListener('keypress', e => {
+    if ( e.keyCode === ENTER_KEY) {
+      search()
+    }
+  })
+})
 </script>
 
 <template>
 <div class="h-12 w-full border-b-[3px] border-black flex justify-end">
-  <input class="w-64 border-l-[3px] border-black px-5" placeholder=" _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" v-model="input"/>
+  <input class="w-64 bg-transparent border-l-[3px] border-black px-5" placeholder=" _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" v-model="input"/>
   <div v-on:click="search" class="w-32 border-l-[3px] border-black grid place-content-center">Search</div>
 </div>
 </template>
