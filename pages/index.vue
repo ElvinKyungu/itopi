6711a5343bd3737c98f2b4c69fbb2385e98a4d0a
maiwenn <maiwenn@soixantecircuits.fr>
2022-07-15 11:07:37 +0200

    :gem: replace fuzzy search with fuse.js

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,38 @@
 <script setup>
 import { useStore } from '../store/index.js'
-import FuzzySearch from 'fuzzy-search'
+import Fuse from 'fuse.js'
 
 const store = useStore()
 
+const options = {
+  isCaseSensitive: false,
+  findAllMatches: true,
+  includeScore: true,
+  shouldSort: true,
+  includeMatches: true,
+  threshold: 0.3,
+  keys:['fields.Name', 'fields.Notes', 'fields.Mots_clefs']
+}
+
+const formatData = (data) => {
+  const newData = []
+  if (Array.isArray(data)) {
+    for (let item of data) {
+      newData.push({ item: item })
+    }
+  }
+  return newData
+}
+
 const filteredData = computed(() => {
   if (store.searchWord === '') {
-    return store.data
+    const result = formatData(store.data)
+    return result
   }
   else {
-    const searcher = new FuzzySearch(
-      store.data,
-      ['fields.Name', 'fields.Notes', 'fields.Mots_clefs'], {
-        caseSensitive: false,
-        sort: true
-    })
-    return searcher.search(store.searchWord)
+    options.minMatchCharLength = store.searchWord.length
+    const fuse = new Fuse(store.data, options)
+    return fuse.search(store.searchWord)
   }
 })
 
@@ -28,11 +45,11 @@ const filteredData = computed(() => {
   <div class="flex flex-wrap justify-around">
     <ProjectCard 
       v-for="project in filteredData"
-      :key="project.id"
-      :title="project.fields.Name"
-      :artiste="project.fields.Notes"
-      :img="project.fields.Attachments[0].url"
-      :tags="project.fields.Mots_clefs"
+      :key="project.item.id"
+      :title="project.item.fields.Name"
+      :artiste="project.item.fields.Notes"
+      :img="project.item.fields.Attachments[0].url"
+      :tags="project.item.fields.Mots_clefs"
     />
     <div v-for="index in 4" :key="index" class="w-64 mx-5"></div>
   </div>
