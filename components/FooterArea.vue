b9a7da64d018bf4cb894cbc4a1c32a63d7712016
maiwenn <maiwenn@soixantecircuits.fr>
2022-07-22 15:22:44 +0200

    :gem: show version

--- components/FooterArea.vue
+++ components/FooterArea.vue
@@ -1,5 +1,12 @@
+<script setup>
+import pkg from '../package.json'
+
+console.log(pkg)
+const version = ref(pkg.version)
+</script>
+
 <template>
-<section id="footer-area" class="h-20 p-5 mb-0">
-  <p class="text-black text-xs">Proposé par <span class="font-semibold text-sm">Itopi</span>, conçu par <a href="https://www.soixantecircuits.fr/" class="text-sm font-semibold underline">Soixante circuits</a></p>
+<section id="footer-area" class="h-20 p-5 mb-0 text-black text-xs">
+  <p>v{{ version }} Proposé par <span class="font-semibold text-sm">Itopi</span>, conçu par <a href="https://www.soixantecircuits.fr/" class="text-sm font-semibold underline">Soixante circuits</a></p>
 </section>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from "nuxt"
 
 export default defineNuxtConfig({
   ssr: false,
