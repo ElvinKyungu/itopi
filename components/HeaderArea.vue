54ba591aa0e309b94e482ac25f39926f0cde12c0
maiwenn <maiwenn@soixantecircuits.fr>
2022-07-25 14:19:53 +0200

    :art: add itopi logo

--- components/HeaderArea.vue
+++ components/HeaderArea.vue
@@ -1,7 +1,13 @@
 <template>
   <section id="header-area" class="h-[700px] bg-bottom bg-cover">
+    <div class="absolute z-10 top-7 inset-x-10 flex items-center">
+      <div class="h-10 w-10">
+        <img src="~/assets/img/itopi-logo.png" class="h-full w-full object-cover object-bottom"/>
+      </div>
+      <p class="pl-4 font-semibold">itopi</p>
+    </div>
     <div class="h-full w-full relative">
-      <img src="~/assets/banner.jpg" class="h-full w-full object-cover object-bottom"/>
+      <img src="~/assets/img/banner.jpg" class="h-full w-full object-cover object-bottom"/>
     </div>
     <div class="h-full w-full flex justify-center pt-32 absolute top-0">
       <h1 class="font-semibold text-black text-center text-4xl md:text-6xl whitespace-pre-wrap"> {{ "1000 installations,\nAccédez à 100 gratuitement !" }}</h1>
