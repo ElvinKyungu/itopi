b857d77273c95ab6464bbd90070736912ce694d8
maiwenn <maiwenn@soixantecircuits.fr>
2022-07-25 12:16:55 +0200

    :art: begin to adapt project view to mobile

--- components/ProjectView.vue
+++ components/ProjectView.vue
@@ -22,18 +22,23 @@ const project = computed(() => {
 </script>
 
 <template>
-<div @click.self="leave" class="h-screen w-screen fixed top-0 z-30 bg-white/50 grid place-content-center" id="project-view">
-  <div v-if="project != null" class="h-[600px] w-[1200px] retalive z-30 bg-white outline outline-2 outline-black rounded-lg grid grid-cols-7 gap-10 px-14 py-12">
-    <div class="h-full w-full col-span-3 rounded-lg outline outline-2 outline-black">
+<div @click.self="leave" class="h-screen w-screen fixed top-0 inset-x-0 z-30 bg-white/50 grid md:place-content-center" id="project-view">
+  <div
+    v-if="project != null"
+    class="h-full md:h-[900px] lg:h-[700px] xl:h-[600px] w-full md:w-[760px] lg:w-[1000px] xl:w-[1200px] relative z-30 bg-white md:outline outline-2 outline-black md:rounded-lg md:grid md:grid-cols-7 md:gap-10 px-6 md:px-14 py-12">
+    <div @click="leave" class="visible md:hidden absolute top-2 right-2 h-10 w-10">
+      <span class="material-symbols-outlined text-3xl">close</span>
+    </div>
+    <div class="h-50 sm:h-80 md:h-full w-full col-span-3 rounded-lg outline outline-2 outline-black">
       <ImageLazy :img="getImg(project)" />
     </div>
-    <div class="h-full w-full col-span-4">
+    <div class="md:h-full w-full col-span-4">
       <div class="h-1/2 flex flex-col justify-between">
-        <h2 class="text-6xl font-medium">{{ project.fields.Name}}</h2>
-        <h3 class="text-4xl font-medium">{{ project.fields.Notes }}</h3>
+        <h2 class="text-xl md:text-6xl font-medium">{{ project.fields.Name}}</h2>
+        <h3 class="text-lg md: text-4xl font-medium">{{ project.fields.Notes }}</h3>
         <div>
-          <p class="text-xl">{{ project.fields.Lieux }}</p>
-          <p class="text-xl">{{ project.fields.Année }}</p>
+          <p class="text-base md:text-xl">{{ project.fields.Lieux }}</p>
+          <p class="text-base md:text-xl">{{ project.fields.Année }}</p>
         </div>
         <div class="flex flex-wrap">
           <CardTag v-for="(tag, index) in project.fields.Mots_clefs" :key="index" :tag="tag" />
@@ -41,7 +46,7 @@ const project = computed(() => {
       </div>
       <div class="h-1/2 flex flex-col justify-between pt-2">
       <h5 class="font-medium">Description</h5>
-      <p class="h-5/6 w-full bg-zinc-100 rounded-lg outline outline-2 outline-black overflow-y-scroll"></p>
+      <p class="min-h-[200px] h-fit md:h-5/6 w-full bg-zinc-100 rounded-lg outline outline-2 outline-black overflow-y-scroll"></p>
       </div>
     </div>
   </div>
