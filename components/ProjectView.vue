d44c66da00983ba460db5a235da833ae8cce9de3
maiwenn <maiwenn@soixantecircuits.fr>
2022-07-25 15:45:28 +0200

    :art: project view description css

--- components/ProjectView.vue
+++ components/ProjectView.vue
@@ -46,7 +46,7 @@ const project = computed(() => {
       </div>
       <div class="h-1/2 flex flex-col justify-between pt-2">
       <h5 class="font-medium">Description</h5>
-      <p class="min-h-[200px] h-fit md:h-5/6 w-full bg-zinc-100 rounded-lg outline outline-2 outline-black overflow-y-scroll">
+      <p class="min-h-[200px] h-fit md:h-5/6 w-full bg-zinc-100 rounded-lg outline outline-2 outline-black overflow-y-scroll p-4 font-medium">
         {{ project.fields.Description }}
       </p>
       </div>
