d8401a34b0194938572893236b46eacc630e390d
maiwenn <maiwenn@soixantecircuits.fr>
2022-08-03 15:46:15 +0200

    :art: add watch effect on lazy image url

--- components/ImageLazy.vue
+++ components/ImageLazy.vue
@@ -14,12 +14,20 @@ const loaded = ref(false)
 if (props.img == null) {
   loaded.value = true
 }
+
+watchEffect(() => {
+  if (props.img == null) {
+    loaded.value = true
+  } else {
+    loaded.value = false
+  }
+})
 </script>
 
 
 <template>
   <div class="h-full w-full bg-slate-100 overflow-hidden" v-bind:class="{ 'animate-pulse': !loaded, 'animate-fade-out-background': loaded }">
-    <img v-if="img != null"
+    <img v-show="img != null"
       :src="img"
       loading="lazy"
       class="h-full w-full object-cover"
