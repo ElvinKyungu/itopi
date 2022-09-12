b2e589fa5477e9036e1b33a34a9a4d568fe2e4f1
Dima-Kr <dmitriy@soixantecircuits.fr>
2022-09-12 11:38:31 +0200

    :art: limit display of tags in list view for better UX

--- components/ListRow.vue
+++ components/ListRow.vue
@@ -55,7 +55,7 @@ const favorite = computed(() =>  store.favoriteArray.includes(props.id))
     <ListTextCase v-on:click="projectPage" class="col-span-2 cursor-pointer font-normal flex flex-wrap" :text="place"/>
     <ListTextCase v-on:click="projectPage" :text="year" class="cursor-pointer font-normal"/>
     <span v-on:click="projectPage" class="col-span-2 border border-black flex flex-wrap items-center overflow-hidden p-2 cursor-pointer gap-2">
-      <CardTag v-for="(tag, index) in tags" :key="index" :tag="tag" mode="card"/>
+      <CardTag v-for="(tag, index) in tags.splice(0, 4)" :key="index" :tag="tag" mode="card"/>
     </span>
     <div class="border border-black grid place-content-center">
       <AddToFavorite :id="id" />
