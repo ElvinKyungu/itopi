2e67407f73034169e6b5de7e2a01ec376d690e6d
Dima-Kr <dmitriy@soixantecircuits.fr>
2022-09-12 11:35:16 +0200

    :art: limit display of tags in each card for better UX

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -33,7 +33,7 @@ const projectPage = () => {
 
 <template>
 <div id="project_card" class="group w-80 h-96 md:w-64 bg-zinc-100 outline outline-2 hover:outline-[3px] outline-black rounded-xl overflow-hidden cursor-pointer">
-  <div class="h-48 md:h-60 w-full relative rounded-t-xl">
+  <div class="h-3/5 md:h-60 w-full relative rounded-t-xl">
     <AddToFavorite :id="id" class="absolute"/>
     <ImageLazy v-on:click="projectPage" :img="img || '../assets/img/no-photo.jpg'" :hover="true"/>
     <div class="absolute z-20 h-5 w-20 rounded-lg border-2  border-black bg-zinc-100 bottom-2 left-2 text-[10px] font-bold font-inter hidden group-hover:flex place-content-center">Voir plus ...</div>
@@ -42,7 +42,7 @@ const projectPage = () => {
     <CardTitle class="font-medium" :title="title"/>
     <CardTitle class="font-semibold" :title="artiste"/>
     <div class="mt-1 flex flex-wrap gap-2 md:gap-1.5">
-      <CardTag v-for="(tag, index) in tags" :key="index" :tag="tag" mode="card"/>
+      <CardTag v-for="(tag, index) in tags.slice(0, 4)" :key="index" :tag="tag" mode="card"/>
     </div>
   </div>
 </div>
